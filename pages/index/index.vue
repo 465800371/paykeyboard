97823d0db931251ada0ea11184389ac4442ef5fd
465800371 <465800371@qq.com>
2019-12-19 16:38:03 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,53 @@
+<template>
+	<view class="content">
+		<button @tap="show = true">弹出键盘</button>
+		<passkeyborad :show="show" @close="close"></passkeyborad>
+	</view>
+</template>
+
+<script>
+	import passkeyborad from '@/components/payPassword/paykeyboard.vue';
+	export default {
+		components: {
+			passkeyborad,
+		},
+		data() {
+			return {
+				show: false,
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			close() {
+				this.show = false
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
